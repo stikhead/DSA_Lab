2d3a27b8d67b443a425d9082d59d4722fe5f5589
stickhead <120650272+stikhead@users.noreply.github.com>
2025-11-20 15:05:46 +0000

    upd

--- 09_stack_array.cpp
+++ 09_stack_array.cpp
@@ -1,16 +1,12 @@
 #include <iostream>
 using namespace std;
-
 class Stack {
-    int arr[100];
-    int top;
-    int size;
+    int arr[100], top, size;
 public:
     Stack(int s) {
         size = s;
         top = -1;
     }
-
     void push(int x) {
         if (top == size - 1) {
             cout << "Stack is full" << endl;
@@ -56,31 +52,12 @@ int main() {
     cout << "Enter stack size: ";
     cin >> n;
     Stack st(n);
-    while (true) {
-        cout << "1 Push" << endl;
-        cout << "2 Pop" << endl;
-        cout << "3 Peek" << endl;
-        cout << "4 Display" << endl;
-        cout << "5 Exit" << endl;
-        cout << "Choose option: ";
-        int choice;
-        cin >> choice;
-        if (choice == 1) {
-            int x;
-            cout << "Enter value to push: ";
-            cin >> x;
-            st.push(x);
-        } else if (choice == 2) {
-            st.pop();
-        } else if (choice == 3) {
-            st.peek();
-        } else if (choice == 4) {
-            st.display();
-        } else if (choice == 5) {
-            break;
-        } else {
-            cout << "Invalid choice" << endl;
-        }
-    }
+    st.push(4);
+    st.push(3);
+    st.push(2);
+    st.push(3);
+    st.pop();
+    st.peek();
+    st.display();
     return 0;
 }
