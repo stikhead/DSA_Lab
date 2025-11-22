2bd1e95ce49d4bb1e77e441e34869764d03f422a
stickhead <120650272+stikhead@users.noreply.github.com>
2025-11-22 03:45:43 +0000

    upd

--- 16_binary_tree_array.cpp
+++ 16_binary_tree_array.cpp
@@ -1,12 +1,56 @@
-#include <iostream>
+// C++ implementation of tree using array
+// numbering starting from 0 to n-1.
+#include<bits/stdc++.h>
 using namespace std;
-int main(){
-    cout<<"Enter number of nodes: ";
-    int n; cin>>n;
-    int tree[100];
-    cout<<"Enter node values: ";
-    for(int i=0;i<n;i++) cin>>tree[i];
-    cout<<"Tree array: ";
-    for(int i=0;i<n;i++) cout<<tree[i]<<(i+1==n? '\n' : ' ');
-    return 0;
+char tree[10];
+int root(char key) {
+  if (tree[0] != '\0'){
+    cout << "Tree already had root\n";
+  }
+  else {
+    tree[0] = key;
+  }
+  return 0;
+}
+
+int set_left(char key, int parent) {
+  if (tree[parent] == '\0')
+    cout << "Can't set child at "<< (parent * 2) + 1;
+  else {
+    tree[(parent * 2) + 1] = key;
+  }
+  return 0;
+}
+
+int set_right(char key, int parent) {
+  if (tree[parent] == '\0')
+    cout << "\nCan't set child at "<< (parent * 2) + 2;
+  else
+    tree[(parent * 2) + 2] = key;
+  return 0;
+}
+
+int print_tree() {
+  cout << "\n";
+  for (int i = 0; i < 10; i++) {
+    if (tree[i] != '\0'){
+         cout << tree[i];
+    }
+    else{
+        cout << "-";
+    }
+  }
+  return 0;
+}
+
+// Driver Code
+int main() {
+  root('A');
+  set_left('B',0);
+  set_right('C', 0);
+  set_left('D', 1);
+  set_right('E', 1);
+  set_right('F', 2);
+  print_tree();
+  return 0;
 }
